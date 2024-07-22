b093223823d41b9649c49cd0c339e0889d690399
Sukant <t-susondhi@microsoft.com>
2024-07-22 23:48:47 +0100

    Learning about home pages in Flask

--- app.py
+++ app.py
@@ -1,6 +1,38 @@
-from flask import Flask
+## Only uncomment if needed
+# import os
+# os.environ['FLASK_APP'] = 'app.py'
+# os.environ['FLASK_ENV'] = 'development'
+
+
+from flask import Flask, render_template
+from flask_restx import Api, Resource  
+
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "<html><body><h1>Hello Pixelator App!</h1></body></html>\n"
+## Uncomment to turn this into an API
+# api = Api(app, version='1.0', title='Pixelator API',
+#           description='A simple Pixelator API')
+
+# ns = api.namespace('api', description='Pixelator operations')
+
+@app.route('/')
+def home():
+    """"Server Home Page"""
+
+    return "<h1>Welcome to the Pixelator App!</h1><p>This is the home page.</p>"
+
+@app.route('/hello')
+class HelloResource(Resource): 
+
+    def get(self):
+        """Returns a greeting"""
+        return {"message": "Hello Pixelator App!"}
+
+@app.route('/goodbye')
+class GoodByeResource(Resource):
+    def get(self):
+        """"Returns a goodbye"""
+        return {"message": "Bye Pixelator"}
+
+if __name__ == '__main__':
+    app.run(debug=True)
